b0269464f24118eb9f87cf07d09acd6a52e9c8c2
Sami azmi <azmis@post.bgu.ac.il>
2021-09-26 12:34:01 +0300

    I managed the files and put them in the right place

--- CV.js
+++
@@ -1,3 +0,0 @@
-function change_text(){
-document.getElementById("Div1").innerHTML = "Azmi Sami";
-}
---
+++ Public/CV.js
@@ -0,0 +1,3 @@
+function change_text(){
+document.getElementById("Div1").innerHTML = "Azmi Sami";
+}
